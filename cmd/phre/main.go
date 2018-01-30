a621283a606be54d59e065f923a1779a7796594c
Brunetto Ziosi <brunetto.ziosi@gmail.com>
2018-01-30 21:53:13 +0100

    :bear: :sparkles: renamig consistently givend the original basename

--- cmd/phre/main.go
+++ cmd/phre/main.go
@@ -1,48 +1,132 @@
 package main
 
 import (
-	"path/filepath"
-	"log"
 	"fmt"
-	"os"
 	"github.com/brunetto/goutils/text"
+	"github.com/pkg/errors"
+	"log"
+	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
+	"sort"
 )
 
-func main () {
-	var (
-		fileNames  []string
-		err        error
-		fileName   string
-		extensions []string
-		ext        string
-		idx        int
-		cwd        string
-		newName    string
-	)
 
-	extensions = []string{"cr2", "CR2", "jpeg", "jpg", "JPEG", "JPG", "tiff", "TIFF", "png", "PNG", "NEF", "nef", "MOV", "mov"}
+type FnData struct {
+	BaseName string
+	Ext      StringSet
+}
 
-	cwd, err = os.Getwd()
+type FnDataSlice []*FnData
+func (a FnDataSlice) Len() int           { return len(a) }
+func (a FnDataSlice) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a FnDataSlice) Less(i, j int) bool { return a[i].BaseName < a[j].BaseName }
+
+type FnDataSet map[string]*FnData
+func NewFnDataSet() FnDataSet {
+	s := FnDataSet{}
+	return s
+}
+func (s FnDataSet) Add (bn, ext string) {
+	if s.Contains(bn) {
+		s[bn].Ext.Add(ext)
+	} else {
+		s[bn] = &FnData{BaseName: bn, Ext: NewStringSet(ext)}
+	}
+}
+
+func (s FnDataSet) Contains(str string ) bool {
+	_, exists := s[str]
+	return exists
+}
+
+func (s FnDataSet) Remove (str string ) {
+		delete(s, str)
+}
+
+func (s FnDataSet) ToSlice () FnDataSlice {
+	sl := FnDataSlice{}
+	for _, v := range s {
+		sl = append(sl, v)
+	}
+	return sl
+}
+
+
+type StringSet map[string]struct{}
+func NewStringSet(strs ...string) StringSet {
+	s := StringSet{}
+	s.Add(strs...)
+	return s
+}
+
+func (s StringSet) Add (strs ...string ) {
+	for _, str := range strs {
+		s[str] = struct{}{}
+	}
+}
+
+func (s StringSet) Contains(str string ) bool {
+	_, exists := s[str]
+	return exists
+}
+
+func (s StringSet) Remove (strs ...string ) {
+	for _, str := range strs {
+		delete(s, str)
+	}
+}
+
+func SplitOnExtension(fn string )(fbn string, ext string) {
+	ext = filepath.Ext(fn)
+	fbn = strings.TrimSuffix(fn, ext)
+	return fbn, ext
+}
+
+func main() {
+	// file type extensions to consider
+	extensions := NewStringSet(".cr2", ".CR2", ".jpeg", ".jpg", ".JPEG", ".JPG", ".tiff", ".TIFF",
+		".png", ".PNG", ".NEF", ".nef", ".dng", ".DNG", ".MOV", ".mov", ".mpeg", ".MPEG", ".mpeg4", ".MPEG4", ".mp4", ".MP4")
+
+	// get current directory name to be used as new base-name
+	cwd, err := os.Getwd()
 	if err != nil {
-		log.Fatal("Error detecting current directory: ", err.Error())
+		log.Fatal(errors.Wrap(err, "can't detect current directory"))
 	}
 	cwd = filepath.Base(cwd)
 
-	for _, ext = range extensions {
-		fileNames, err = filepath.Glob("*." + ext)
-		for idx, fileName = range fileNames {
-			if err != nil{
-				log.Fatal("Error listing directory files: " + err.Error())
-			}
-			newName = cwd + "-" + text.LeftPad(strconv.Itoa(idx), "0", 4) + "." + ext
-			fmt.Printf("Renaming %v into %v\n", fileName, newName)
-			err = os.Rename(fileName, newName)
+	// get all the file names in the current directory
+	fns, err := filepath.Glob("./*")
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "can't list files"))
+	}
+
+	// create a map (no repetition) with the old base-names and the related existing extensions
+	fnsMap := NewFnDataSet()
+	for _, fn := range fns {
+		fbn, ext := SplitOnExtension(fn)
+		if !extensions.Contains(ext){
+			continue
+		}
+		fnsMap.Add(fbn, ext)
+	}
+
+	// convert map to slice and sort it
+	fnSlice := fnsMap.ToSlice()
+	sort.Sort(fnSlice)
+
+	// for each old base-name and extension, rename
+	padlenght := len(strconv.Itoa(len(fnSlice)))+1
+	for i, fnd := range fnSlice {
+		for ext, _ := range fnd.Ext {
+			oldName := fnd.BaseName + ext
+			newName := cwd + "-" + text.LeftPad(strconv.Itoa(i), "0", padlenght) + ext
+			fmt.Printf("Renaming %v into %v\n", oldName, newName)
+			err = os.Rename(oldName, newName)
 			if err != nil {
-				log.Fatal("Error renaming file: " + err.Error())
+				log.Fatal(errors.Wrap(err, "can't rename file"))
 			}
 		}
 	}
 }
-
-
--- cmd/phre/main_test.go
+++ cmd/phre/main_test.go
@@ -1,1 +1,117 @@
 package main
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"sort"
+)
+
+func TestNewSet(t *testing.T) {
+	s := NewStringSet("t1", "t2")
+	assert.True(t, s.Contains("t1"))
+	assert.True(t, s.Contains("t2"))
+}
+
+func TestSet_AddSingle(t *testing.T) {
+	s := StringSet{}
+	s.Add("t1")
+	_, exists := s["t1"]
+	assert.True(t, exists)
+}
+
+func TestSet_AddSingleTwice(t *testing.T) {
+	s := StringSet{}
+	s.Add("t1")
+	s.Add("t2")
+	assert.Equal(t, 2, len(s))
+	_, exists := s["t1"]
+	assert.True(t, exists)
+	_, exists = s["t2"]
+	assert.True(t, exists)
+}
+
+func TestSet_AddMultiple(t *testing.T) {
+	s := StringSet{}
+	s.Add("t1", "t2")
+	_, exists := s["t1"]
+	assert.True(t, exists)
+	_, exists = s["t2"]
+	assert.True(t, exists)
+}
+
+func TestSet_Contains(t *testing.T) {
+	s := StringSet{}
+	s.Add("t1")
+	assert.True(t, s.Contains("t1"))
+	assert.False(t, s.Contains("t2"))
+}
+
+func TestSet_RemoveSingle(t *testing.T) {
+	s := StringSet{}
+	s.Add("t1")
+	s.Remove("t1")
+	_, exists := s["t1"]
+	assert.False(t, exists)
+}
+
+func TestSet_RemoveMultiple(t *testing.T) {
+	s := StringSet{}
+	s.Add("t1")
+	s.Add("t2")
+	s.Add("t3")
+	s.Remove("t1", "t2")
+	_, exists := s["t1"]
+	assert.False(t, exists)
+	_, exists = s["t2"]
+	assert.False(t, exists)
+	_, exists = s["t3"]
+	assert.True(t, exists)
+}
+
+func TestSplitOnExtension(t *testing.T) {
+	fbn, ext := SplitOnExtension("fbn.ext")
+	assert.Equal(t, "fbn", fbn)
+	assert.Equal(t, ".ext", ext)
+}
+
+func TestNewFnDataSet(t *testing.T) {}
+
+func TestFnDataSet_Add(t *testing.T) {
+	s := NewFnDataSet()
+	s.Add("t1", ".e1")
+	s.Add("t1", ".e1")
+	s.Add("t1", ".e2")
+	s.Add("t2", ".e1")
+	assert.Equal(t, 2, len(s))
+	t1, exists := s["t1"]
+	assert.True(t, exists)
+	assert.Equal(t, 2, len(t1.Ext))
+	t2, exists := s["t2"]
+	assert.True(t, exists)
+	assert.Equal(t, 1, len(t2.Ext))
+}
+func TestFnDataSet_Contains(t *testing.T) {
+	s := NewFnDataSet()
+	s.Add("t1", ".e1")
+	assert.True(t, s.Contains("t1"))
+	assert.False(t, s.Contains("t2"))
+}
+func TestFnDataSet_Remove(t *testing.T) {
+	s := NewFnDataSet()
+	s.Add("t1", ".e1")
+	s.Remove("t1")
+	assert.False(t, s.Contains("t1"))
+}
+func TestFnDataSet_ToSlice_and_Sort(t *testing.T) {
+	s := NewFnDataSet()
+	s.Add("t1", ".e1")
+	s.Add("t2", ".e2")
+
+	ss := s.ToSlice()
+	sort.Sort(ss)
+
+	assert.Equal(t, 2, len(ss))
+	assert.EqualValues(t, &FnData{BaseName:"t1", Ext:StringSet{".e1": struct{}{}}}, ss[0])
+	assert.EqualValues(t, &FnData{BaseName:"t2", Ext:StringSet{".e2": struct{}{}}}, ss[1])
+
+}
